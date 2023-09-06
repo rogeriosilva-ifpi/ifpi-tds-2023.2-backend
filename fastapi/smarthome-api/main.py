832ba9cd8fc19cd87d94084f295518bb208bbca7
Rogério Silva <rogerio410@gmail.com>
2023-09-06 17:57:08 -0300

    Finish Database

--- fastapi/smarthome-api/main.py
+++ fastapi/smarthome-api/main.py
@@ -4,15 +4,16 @@ from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy import table
-from sqlmodel import Field, Session, SQLModel, create_engine, delete, select
+from sqlmodel import (Field, Relationship, Session, SQLModel, create_engine,
+                      delete, select, update)
 from ulid import ulid
 
 app = FastAPI()
 
 # SQL Model
 # db_url = 'sqlite:///database.db'
-# db_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/smarthome'
-db_url = 'postgresql+psycopg2://smarthome_b2hz_user:pmtszsfMU1fKevP6GYrznZdvJHxX2007@dpg-cjsd4rktjf3s73biv9tg-a.oregon-postgres.render.com/smarthome_b2hz'
+db_url = 'postgresql+psycopg2://postgres:postgres@localhost:5432/smarthome'
+# db_url = 'postgresql+psycopg2://smarthome_b2hz_user:pmtszsfMU1fKevP6GYrznZdvJHxX2007@dpg-cjsd4rktjf3s73biv9tg-a.oregon-postgres.render.com/smarthome_b2hz'
 # postgres://user:password@host/db
 
 engine = create_engine(db_url, echo=True)
@@ -36,19 +37,26 @@ ambientes = []
 ambiente_id = 0
 dispositivo_id = 0
 
-class Dispositivo(BaseModel):
-    id: str | None = None
-    description: str
-    icone: int | None
-    estado_conexao: str | None
-    status: bool | None
-
 
 class Ambiente(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     descricao: str
     icone: str | None = Field(default='icone.png')
-    # items: list[Dispositivo] = []
+    dispositivos: list['Dispositivo'] = Relationship(back_populates='ambiente')
+
+
+class Dispositivo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    description: str
+    icone: str | None = Field(default='icone.png')
+    estado_conexao: bool | None = Field(default=True)
+    status: bool | None = Field(default=False)
+
+    ambiente_id: int | None = Field(default=None, foreign_key="ambiente.id")
+    ambiente: Ambiente | None = Relationship(back_populates='dispositivos')
+
+
+
 
 SQLModel.metadata.create_all(engine)
 
@@ -57,6 +65,7 @@ def buscar_ambiente_por_id(id: int):
     session = Session(engine)
     instrucao = select(Ambiente).where(Ambiente.id == id)
     ambiente = session.exec(instrucao).first()
+    session.close()
     return ambiente
 
 @app.get('/ambientes', status_code=status.HTTP_200_OK)
@@ -64,6 +73,8 @@ def show_ambiente():
     session = Session(engine)
     instrucao = select(Ambiente)
     ambientes = session.exec(instrucao).fetchall()
+    session.close()
+
     return ambientes
 
 @app.post('/ambientes', status_code=status.HTTP_201_CREATED)
@@ -72,10 +83,11 @@ def criar_ambiente(ambiente: Ambiente):
     session.add(ambiente)
     session.commit()
     session.refresh(ambiente)
+    session.close()
     return ambiente
 
 @app.put('/ambientes/{id}', status_code=status.HTTP_200_OK)
-def atualizar_ambientes(id: str, ambiente: Ambiente):
+def atualizar_ambientes(id: int, ambiente: Ambiente):
     ambiente_atual = buscar_ambiente_por_id(id)
 
     # Fail Fast
@@ -83,6 +95,10 @@ def atualizar_ambientes(id: str, ambiente: Ambiente):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Ambiente não encontrado')
     
     ambiente_atual.descricao = ambiente.descricao
+    session = Session(engine)
+    session.add(ambiente_atual)
+    session.commit()
+    session.close()
 
     return ambiente_atual
     
@@ -101,56 +117,96 @@ def remover_ambiente(id: int):
     instrucao = delete(Ambiente).where(Ambiente.id == id)
     session.exec(instrucao)
     session.commit()
+    session.close()
 
 '''Área Dispositivo'''
+
+@app.get('/ambientes/{ambiente_id}/dispositivos')
+def obter_dispositivos(ambiente_id: int):
+    ambiente_atual = buscar_ambiente_por_id(ambiente_id)
+
+    # Fail Fast
+    if not ambiente_atual:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Ambiente não encontrado')
+
+    # Buscar dispositivos
+    session = Session(engine)
+    inst = select(Dispositivo)\
+        .where(Dispositivo.ambiente_id == ambiente_id)
+    
+    dispositivos = session.exec(inst).fetchall()
+    session.close()
+    return dispositivos
+
+
 @app.post('/ambientes/{id}/dispositivos')
-def adicionar_dispositivo(id: str, dispositivo: Dispositivo):
+def adicionar_dispositivo(id: int, dispositivo: Dispositivo):
     ambiente = buscar_ambiente_por_id(id)
 
     if not ambiente:
         raise HTTPException(status_code=404, detail='Ambiente não encontrado')
     
-    dispositivo.id = ulid()
-    ambiente.items.append(dispositivo)
-    return ambiente.items
+    dispositivo.ambiente_id = id
+    session = Session(engine)
+    session.add(dispositivo)
+    session.commit()
+    session.refresh(dispositivo)
+    session.close()
+
+    return dispositivo
 
 
 @app.delete('/ambientes/{ambiente_id}/dispositivos/{dispositivo_id}')
-def remover_dispositivo(ambiente_id: str, dispositivo_id: str):
+def remover_dispositivo(ambiente_id: int, dispositivo_id: int):
     ambiente = buscar_ambiente_por_id(ambiente_id)
 
     if not ambiente:
         raise HTTPException(status_code=404, detail='Ambiente não localizado!')
 
 
-    dispositivo = buscar_dispositivo_por_id(dispositivo_id, ambiente)
+    dispositivo = buscar_dispositivo_por_id(dispositivo_id)
 
-    if not dispositivo:
+    if not dispositivo or dispositivo.ambiente_id != ambiente.id:
         raise HTTPException(status_code=404, detail='Dispositivo não localizado!')
     
-    ambiente.items.remove(dispositivo)
+    session = Session(engine)
+    instrucao = delete(Dispositivo).where(Dispositivo.id == dispositivo_id)
+    session.exec(instrucao)
+    session.commit()
+    session.close()
 
 
-def buscar_dispositivo_por_id(id, ambiente):
-    for item in ambiente.items:
-        if item.id == id:
-            return item
-    return None
+def buscar_dispositivo_por_id(id: int):
+    session = Session(engine)
+    instrucao = select(Dispositivo).where(Dispositivo.id == id)
+    dispositivo = session.exec(instrucao).first()
+    session.close()
+    return dispositivo
 
 @app.put('/ambientes/{origem_id}/dispositivos/{dispositivo_id}/mover/{destino_id}')
-def mover_dispositivo(origem_id: str, dispositivo_id: str, destino_id: str):
+def mover_dispositivo(origem_id: int, dispositivo_id: int, destino_id: int):
+
+    dispositivo = buscar_dispositivo_por_id(dispositivo_id)
+    
+    if not dispositivo:
+        raise HTTPException(status_code=404, detail="Dispositivo não localizado!")
+
     ambiente_origem = buscar_ambiente_por_id(origem_id)
     ambiente_destino = buscar_ambiente_por_id(destino_id)
 
     if not ambiente_origem or not ambiente_destino:
         raise HTTPException(status_code=404, detail="Ambiente Origem/Destino não localizado!")
     
-    dispositivo = buscar_dispositivo_por_id(dispositivo_id, ambiente_origem)
+    if ambiente_origem.id == ambiente_destino.id:
+        raise HTTPException(status_code=400, detail="Ambiente de destino deve diferente do de origem!")
+    
+    
+    dispositivo.ambiente_id = ambiente_destino.id
 
-    if not dispositivo:
-        raise HTTPException(status_code=404, detail="Dispositivo não localizado!")
+    session = Session(engine)
+    session.add(dispositivo)
+    session.commit()
+    session.close()
 
-        
-    ambiente_origem.items.remove(dispositivo)
-    ambiente_destino.items.append(dispositivo)
+    return dispositivo
     
