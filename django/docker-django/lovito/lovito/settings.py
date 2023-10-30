c489bd77050745816c1d6ddee38c5cbb7d0cf0f5
Rogério Silva <rogerio410@gmail.com>
2023-10-30 11:39:45 -0300

    remove teste

--- django/docker-django/lovito/lovito/settings.py
+++ django/docker-django/lovito/lovito/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
 	# }
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': config('DB_NAME', default='teste'),
+		'NAME': config('DB_NAME'),
 		'USER': config('DB_USER'),
 		'PASSWORD': config('DB_PASS'),
 		'HOST': config('DB_HOST'),
