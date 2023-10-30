3593063ba563c1dc53ea08164d07e5781e5f1a30
Rogério Silva <rogerio410@gmail.com>
2023-10-30 10:24:46 -0300

    Added Database config

--- django/docker-django/lovito/lovito/settings.py
+++ django/docker-django/lovito/lovito/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'lovito.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+	# 'default': {
+	# 	'ENGINE': 'django.db.backends.sqlite3',
+	# 	'NAME': BASE_DIR / 'db.sqlite3',
+	# }
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': config('DB_NAME'),
+		'USER': config('DB_USER'),
+		'PASSWORD': config('DB_PASS'),
+		'HOST': config('DB_HOST'),
+		'PORT': config('DB_PORT'),
 	}
 }
 
